5334d9005557f15c93782ee496a5e036255451e3
Lance Vincent <lancevincent@gmail.com>
2015-08-31 23:43:57 +0800

    added left layout - category menus

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,13 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @articles = Article.all.order('created_at DESC')    
+    if params[:category]
+      @category_id = Category.find_by(name: params[:category]).id
+
+      @articles = Article.where(category_id: @category_id).order('created_at DESC')
+    else
+      @articles = Article.all.order('created_at DESC')
+    end
   end
 
   def new
